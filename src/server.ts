55c96deeec6dc10d3400cf6b8abc5af02df4d2a4
Daniel Errington <derrington@gmail.com>
2023-08-28 14:33:16 +0100

    Get server running in Windows

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT ?? 8080;
 
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
     response.status(200).send("");
 });
 
