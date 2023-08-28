4c6a5acda841fcf6fd3d7450216851f79ccf0232
Daniel Errington <derrington@gmail.com>
2023-08-28 12:25:25 +0100

    Remove original CPP code

--- ui/main.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-ReactDOM.render((
-    <>
-        Hello, World! { Math.floor(Math.random() * 100) }
-    </>),
-    document.getElementById("react-root"))
